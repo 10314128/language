8804199cbcf846646988fc7bc3d5b0168f1b4e33
m516 <m516@m516>
2015-10-23 15:22:59 +0800

    !!!!!!

---
+++ language/main/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+import datetime
+
+def main(request):
+    context = {'message':'Django 很棒', 'time':datetime.datetime.now()}
+    return render(request, 'main/main.html', context)
---
+++ language/wiki/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def wiki(request):
+    return render(request, 'wiki/wiki.html')
+
+def about(request):
+    return render(request, 'wiki/about.html')
