d77285da1690320651d7c44dadbc7dd94d9a611c
m516 <m516@m516>
2015-12-11 22:41:27 +0800

    9

--- language/wiki/urls.py
+++ language/wiki/urls.py
@@ -3,4 +3,12 @@ from wiki import views
 
 urlpatterns = [
     url(r'^$', views.wiki, name='wiki'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^category/(?P<categoryName>[\w\-]+)/$', views.category, name='category'),
+    url(r'^addCategory/$', views.addCategory, name='addCategory'),
+    url(r'^addPage/(?P<categoryName>[\w\-]+)/$', views.addPage, name='addPage'),
+    url(r'^deletePage/(?P<pageID>[0-9]+)/$', views.deletePage,
+        name='deletePage'),
+    url(r'^deleteCategory/(?P<categoryID>[0-9]+)/$', views.deleteCategory,
+        name='deleteCategory'),
 ]
